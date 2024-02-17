a2fcafd4d64865290733534936ff5683601d2a42
Croco-Kyle <8kwest@gmail.com>
2024-02-17 15:17:15 -0700

    creating player weapon subclass
    modularizing player weapons

--- main.py
+++ main.py
@@ -6,14 +6,18 @@ from player import Player
 
 # pygame setup
 pygame.init()
-bg = pygame.image.load("images\\background.jpg")
-screen = pygame.display.set_mode((1280, 720))
+bg = pygame.image.load("images/background.jpg")
+screen = pygame.display.set_mode((1920, 1080))
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
 # initializing objects
+all_sprites = pygame.sprite.Group() # create a group for all sprites
+
 player = Player(screen)
+all_sprites.add(player) # add the player to the group
+
 
 while running:
     # poll for events
@@ -30,7 +34,10 @@ while running:
     # Create collisiong checking for the player here
     player.move(key, dt)
     screen.fill("purple")
-    player.draw()
+
+    # sprite management
+    all_sprites.update()
+    all_sprites.draw(screen)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
--- player.py
+++ player.py
@@ -3,33 +3,48 @@ import pygame
 
 
 
-class Player: 
+class Player(pygame.sprite.Sprite): 
  
 
     def __init__(self, screen):
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
         self.screen = screen
-        self.image = pygame.image.load("player.png")
-        self.player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+        self.image = pygame.image.load("images/player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (screen.get_width() / 2, screen.get_height() / 2)
+        
+        
+        # self.player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 
 
-    def update_pos(self, coord_x, coord_y):
-        self.player_pos = pygame.Vector2(coord_x, coord_y)
+    # def update_pos(self, coord_x, coord_y):
+    #     self.player_pos = pygame.Vector2(coord_x, coord_y)
    
 
-    def draw(self):
-        self.screen.blit(self.image , self.player_pos)
-                         
+    # def draw(self):
+    #     self.screen.blit(self.image , self.player_pos)
+                        
     
 
-    
-
+    # def move(self, keys, dt):
+    #     if keys[pygame.K_w]:
+    #         self.player_pos.y -= 300 * dt
+    #     if keys[pygame.K_s]:
+    #         self.player_pos.y += 300 * dt
+    #     if keys[pygame.K_a]:
+    #         self.player_pos.x -= 300 * dt
+    #     if keys[pygame.K_d]:
+    #         self.player_pos.x += 300 * dt
+        
     def move(self, keys, dt):
         if keys[pygame.K_w]:
-            self.player_pos.y -= 300 * dt
+            self.rect.y -= 300 * dt
         if keys[pygame.K_s]:
-            self.player_pos.y += 300 * dt
+            self.rect.y += 300 * dt
         if keys[pygame.K_a]:
-            self.player_pos.x -= 300 * dt
+            self.rect.x -= 300 * dt
         if keys[pygame.K_d]:
-            self.player_pos.x += 300 * dt
+            self.rect.x += 300 * dt
