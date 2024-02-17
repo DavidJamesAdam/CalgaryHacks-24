6e51b9d535b247418000f7dcc2e052be8ca0681f
Croco-Kyle <8kwest@gmail.com>
2024-02-17 12:36:10 -0700

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -1,3 +1,6 @@
 # enemies class. 
 # This class is used to create the enemies in the game. Drawing, 
 # drawing, and updating the enemies is done in this class.
+
+# Enemy ai goes towards the player and "attacks" the player
+# by colliding with the player.
