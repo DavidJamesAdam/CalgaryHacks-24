c9a4be19b05340500353ce122f62b66747bc7b32
NeptunesOrcaII <orcabot002@gmail.com>
2024-02-17 14:56:50 -0700

    Level now draws

--- levelList.py
+++ levelList.py
@@ -1,14 +1,29 @@
 import pygame
 # The list of levels available to load
-screen = pygame.get_window_size()
-wallStartSize = 50
 
-# level 1
-topWall = pygame.Rect(0,0,screen[0],wallStartSize)
-leftWall = pygame.Rect(0,0,wallStartSize,screen[1])
-bottomWall = pygame.Rect(0,screen[1],screen[0],wallStartSize)
-rightWall = pygame.Rect(screen[0],0,wallStartSize,screen[1])
-level1 = [topWall, rightWall, leftWall, bottomWall]
+class LevelList:
+    def __init__(self,screenSize,wallStartSize):
+        wallStartSize
 
-#all levels
-levels = [level1]
+        # level 1
+        topWall = pygame.Rect(0,0,screenSize[0],wallStartSize)
+        leftWall = pygame.Rect(0,0,wallStartSize,screenSize[1])
+        bottomWall = pygame.Rect(0,screenSize[1]-wallStartSize,screenSize[0],wallStartSize)
+        rightWall = pygame.Rect(screenSize[0]-wallStartSize,0,wallStartSize,screenSize[1])
+        level1 = [topWall, rightWall, leftWall, bottomWall]
+
+        # level 2
+
+
+        # all levels
+        self.levels = [level1]
+
+    def numLevels(self) :
+        return len(self.levels)
+        
+    def getLevel(self, levelNumber):
+        if levelNumber >= self.numLevels() :
+            print("%d is not a valid level! Returning level[0] from getLevel() instead.")
+            return self.levels[0]
+        else :
+            return self.levels[levelNumber]
--- levelManager.py
+++ levelManager.py
@@ -1,40 +1,51 @@
 import pygame
-import levelList
+from levelList import LevelList
 # Level design class.
 # Level drawing, updating, and collision detection done in this class.
 
-screen = pygame.get_window_size()
+class LevelManager:
+    def __init__(self,surface, wallShade, wallThicc):
+        self.surface = surface
+        self.screenSize = surface.get_size()
+        self.wallColour = wallShade
+        self.wallThickness = wallThicc
+        
+        # level is made of a series of boxes, each of which can increase/decrease in size, and also a background colour
+        self.levelRectangles = []
 
-# level is made of a series of boxes, each of which can increase/decrease in size, and also a background colour
-levelRectangles = []
+        #levelList
+        self.lvls = LevelList(self.screenSize,self.wallThickness)
 
+    # loads a level from a set of rectangles
+    def loadLevel(self, rectList):
+        self.levelRectangles = rectList
 
-# loads a level from a set of rectangles
-def loadLevel(rectList):
-    levelRectangles = rectList
+    def loadLevel(self, levelNum):
+        if (self.lvls.numLevels() < levelNum) :
+            self.levelRectangles = self.lvls.getLevel(levelNum)
+        else :
+            self.levelRectangles = self.lvls.getLevel(0)
 
-def loadLevel(levelNum):
-    if (len(levelList.levels) < levelNum) :
-        levelRectangles = levelList.levels[levelNum]
-    else :
-        levelRectangles = levelList.levels[0]
+    # draw level
+    def drawLevel(self):
+        for box in self.levelRectangles:
+            pygame.draw.rect(self.surface, self.wallColour, box)
 
-# draw level
-# for all boxes in level
-# draw the box
+    # for all boxes in level
+    # draw the box
 
-# check collisions with level
-# takes in object (e.g. enemy, player) location & size as argument
-# spits out if collision happens as result
-# check the object vs all the rectangles, quick return if true on any of them
-# if (obj Right => box Left || obj Leftt <= box Right) && (obj Top => box Bottom || obj Bottom <= box Top)
-# collision, return true
-# else next box
+    # check collisions with level
+    # takes in object (e.g. enemy, player) location & size as argument
+    # spits out if collision happens as result
+    # check the object vs all the rectangles, quick return if true on any of them
+    # if (obj Right => box Left || obj Leftt <= box Right) && (obj Top => box Bottom || obj Bottom <= box Top)
+    # collision, return true
+    # else next box
 
-# update level
-# takes in size change
-# for all boxes in level
-# change size of boxes by indicated factor (with at least one pixel of width)
-# later feature to add move the door if it collides with any of the rectangles, if it does
+    # update level
+    # takes in size change
+    # for all boxes in level
+    # change size of boxes by indicated factor (with at least one pixel of width)
+    # later feature to add move the door if it collides with any of the rectangles, if it does
 
 
