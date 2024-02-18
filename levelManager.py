7e04f48505a9a4e0ab0817a9582d571b65e74be0
NeptunesOrcaII <orcabot002@gmail.com>
2024-02-17 22:05:37 -0700

    Added documentation, fixed updateLevel() to work with negative numbers

--- levelManager.py
+++ levelManager.py
@@ -65,24 +65,23 @@ class LevelManager:
         # if updatePx is too small, then don't update it this frame.
         # Instead, increment a counter that triggers adding one pixel at a future frame
         # This makes the average pixel increase/frame be the set wallUpdateRate
-        if (updatePx < 1):
+        if ((updatePx < 1) & (updatePx > -1)):
             self.wallUpdateTick += updatePx
             updatePx = 0
 
         # If the counter gets high enough, trigger adding one pixel to the walls in this frame, and then reset the counter
         if (self.wallUpdateTick > 2):
-            updatePx += 1
+            updatePx = 1
+            self.wallUpdateTick = 0
+        elif (self.wallUpdateTick < -2):
+            updatePx = -1
             self.wallUpdateTick = 0
 
         # for all boxes in the level
         for box in self.levelRectangles:
             resizeWall(box, updatePx)
 
-    # update level
-    # takes in size change
-    # for all boxes in level
-    # change size of boxes by indicated factor (with at least one pixel of width)
-    # later feature to add move the door if it collides with any of the rectangles, if it does
+    # later feature to add move the door, maybe other objects if it collides with any of the rectangles, if it does
 
 #uniformly changes the size of a wall (a rectangle) such that it maintains its centrepoint
 def resizeWall(wall, sizeChange):
